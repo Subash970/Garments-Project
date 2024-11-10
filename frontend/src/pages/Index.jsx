280e873305eaef4c8773a87d3975b13793131291
Subash970 <2005subashk@gmail.com>
2024-11-10 08:08:59 +0530

    pagination issue fixed on index page

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -10,8 +10,8 @@ const Index = () => {
 
   const handleScroll = () => {
     if (
-      window.innerHeight + document.documentElement.clientHeight + 1 >
-      document.documentElement.scrollTop
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.scrollHeight - 1
     ) {
       setPage((prev) => prev + 1);
     }
@@ -23,6 +23,8 @@ const Index = () => {
   }, []);
 
   const getBills = useCallback(async () => {
+    if (loading) return;
+
     setLoading(true);
     setMsg(null);
     try {
@@ -32,23 +34,32 @@ const Index = () => {
           headers: { authorization: localStorage.getItem("token") },
         }
       );
-      setBills((prev) => [...prev, ...response.data.bills]);
+      const newBills = response.data.bills;
+      setBills((prevBills) => {
+        const allBills = [...prevBills, ...newBills];
+
+        const uniqueBills = allBills.filter(
+          (value, index, self) =>
+            index === self.findIndex((t) => t._id === value._id)
+        );
+        return uniqueBills;
+      });
     } catch (err) {
-      setMsg(err.response?.data?.msg || "an error occured. please try again");
+      setMsg(err.response?.data?.msg || "An error occurred. Please try again.");
     } finally {
       setLoading(false);
     }
-  }, [page]);
+  }, [page, loading]);
 
   useEffect(() => {
     getBills();
   }, [page, getBills]);
 
   const BillsDom = () => {
-    return bills.map((bill, i) => (
+    return bills.map((bill) => (
       <div
         className="bills d-flex align-items-center border-bottom px-3 my-4"
-        key={i}
+        key={bill._id}
       >
         <a href={`/bill/${bill._id}`}>
           <div>
@@ -65,7 +76,7 @@ const Index = () => {
     <>
       <p className="h6 text-danger px-3">{msg}</p>
       <div className="d-flex flex-column mx-4 mt-5">
-        {bills.length > 0 ? BillsDom() : <p></p>}
+        {bills.length > 0 ? BillsDom() : <p>No bills available.</p>}
       </div>
       <div className="px-4" style={{ height: "20px" }}>
         {loading && <p>Loading...</p>}
